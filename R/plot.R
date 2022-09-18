a5c8638cba063957b411deed872d530c4c1c7168
kerni714 <kerni714@student.liu.se>
2022-09-18 21:12:06 +0200

    Update R functions linreg_qr and plot

--- R/plot.R
+++ R/plot.R
@@ -23,21 +23,23 @@ plot.linreg <- function(obj) {
   cpt <- paste0("linreg(",format(frm),")")
 
   #- Calculate summary statistics
-  resid_means <- aggregate(df$resid, list(df$pred), FUN=mean)
+  resid_means <- stats::aggregate(df$resid, list(df$pred), FUN=mean)
   names(resid_means) <- c("pred","resid")
-  resid_medians <- aggregate(df$resid, list(df$pred), FUN=median)
+  resid_medians <- stats::aggregate(df$resid, list(df$pred), FUN=median)
   names(resid_medians) <- c("pred","resid")
 
-  resid_std_abs_sqrt_means <- aggregate(df$resid_std_abs_sqrt, list(df$pred), FUN=mean)
+  resid_std_abs_sqrt_means <- stats::aggregate(df$resid_std_abs_sqrt, 
+                                               list(df$pred), FUN=mean)
   names(resid_std_abs_sqrt_means) <- c("pred","resid_std_abs_sqrt")
-  resid_std_abs_sqrt_medians <- aggregate(df$resid_std_abs_sqrt, list(df$pred), FUN=median)
+  resid_std_abs_sqrt_medians <- stats::aggregate(df$resid_std_abs_sqrt, 
+                                                 list(df$pred), FUN=median)
   names(resid_std_abs_sqrt_medians) <- c("pred","resid_std_abs_sqrt")
 
   #----------------------------------------------------------------------------#
   #- Make plots
   #----------------------------------------------------------------------------#
 
-  theme_set(theme_bw())
+  ggplot2::theme_set(ggplot2::theme_bw())
   #- Plot 1
 
   pl1 <-
@@ -47,33 +49,35 @@ plot.linreg <- function(obj) {
 
     ggplot2::ylim(round(min(df$resid),2), round(max(df$resid),2)) +
 
-    ggplot2::geom_text(aes(label=ifelse(resid_std_abs_sqrt>=third_topval,
-              as.character(id),'')), hjust=-0.2, vjust=-0.2, check_overlap = TRUE,
+    ggplot2::geom_text(ggplot2::aes(label=ifelse(resid_std_abs_sqrt>=third_topval,
+              as.character(df$id),'')), hjust=-0.2, vjust=-0.2, check_overlap = TRUE,
               size=3) +
 
-    ggplot2::geom_line(data=resid_means, aes(color="mean")) +
-    ggplot2::geom_line(data=resid_medians, aes(color="median")) +
+    ggplot2::geom_line(data=resid_means, ggplot2::aes(color="mean")) +
+    ggplot2::geom_line(data=resid_medians, ggplot2::aes(color="median")) +
     ggplot2::scale_color_manual(name = "", values = c("mean" = "red",
              "median" = "darkblue")) +
 
     ggplot2::labs(title="Residuals Vs Fitted",
                y="Residuals", x="Fitted values", caption=paste0(cpt)) +
     ggplot2::theme(plot.title = element_text(hjust = 0.5),
-          plot.caption = element_text(hjust = 0.5))
+          plot.caption = ggplot2::element_text(hjust = 0.5))
 
   plot(pl1)
 
   #- Plot 2
-  pl2 <- ggplot2::ggplot(data = df) + ggplot2::aes(x=pred, y=resid_std_abs_sqrt) +
+  pl2 <- ggplot2::ggplot(data = df) + 
+    ggplot2::aes(x=pred, y=resid_std_abs_sqrt) +
     ggplot2::geom_point(shape = 1) +
 
     ggplot2::ylim(0, round(max(resid_std_abs_sqrt),1)) +
 
-    ggplot2::geom_text(aes(label=ifelse((resid_std_abs_sqrt>=third_topval) ,as.character(id),'')),
-              hjust=-0.2, vjust=-0.2, check_overlap = TRUE, size=3) +
+    ggplot2::geom_text(ggplot2::aes(label=ifelse((resid_std_abs_sqrt>=third_topval),
+              as.character(id),'')),hjust=-0.2, vjust=-0.2, check_overlap = TRUE,
+              size=3) +
 
-    ggplot2::geom_line(data=resid_std_abs_sqrt_means, aes(color="mean")) +
-    ggplot2::geom_line(data=resid_std_abs_sqrt_medians, aes(color="median")) +
+    ggplot2::geom_line(data=resid_std_abs_sqrt_means, ggplot2::aes(color="mean")) +
+    ggplot2::geom_line(data=resid_std_abs_sqrt_medians, ggplot2::aes(color="median")) +
     ggplot2::scale_color_manual(name = "", values = c("mean" = "red",
             "median" = "darkblue")) +
 
