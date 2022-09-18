ea355177e39098eef6194c48cf3e1481e08b8593
AlanCT94 <caciqueta94@gmail.com>
2022-09-18 19:41:45 +0200

    Coefficients instead of coefficients variance

--- R/methods1.R
+++ R/methods1.R
@@ -4,7 +4,7 @@ print.linreg <- function(obj){
   cat("linreg(",format(obj$formula),",data=",obj$data1,")","\n")
   cat("\n")
   cat("Coefficients:","\n")
-  print(obj$coefficients_variance)
+  print(obj$coefficients)
 }
 resid.linreg <- function(obj){
 
